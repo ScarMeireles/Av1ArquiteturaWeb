68bedc7b5a0e46dff2ca7059c90471c0e1eecd00
Arthur Meireles <114191275+ScarMeireles@users.noreply.github.com>
2025-04-25 00:01:25 -0300

    Add files via upload

---
+++ Av1ArquiteturaWeb/democrud/src/main/java/com/example/democrud/controller/HelloController.java
@@ -0,0 +1,25 @@
+
+package com.example.democrud.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.web.bind.annotation.RestController; 
+import com.example.democrud.service.HelloService;
+
+
+@RestController
+
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private HelloService helloservice;
+
+
+    @GetMapping
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return helloservice.getHello(name);
+    }
+}
